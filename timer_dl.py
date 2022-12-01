302a3950bfb9f878d06a0ddb6a84de9ba10868e6
muririn <ki-nismr@ist.osaka-u.ac.jp>
2022-12-01 14:21:28 +0900

    complete ver1.0

--- timer_dl.py
+++ timer_dl.py
@@ -2,56 +2,71 @@
 from keras.models import load_model
 import numpy as np
 import cv2
+import pyocr
+from PIL import Image
+
+sec = {}
+
+tick = 1.0/30.0
 
 # カメラから画像を取得して,リアルタイムに手書き数字を判別させる。
 # 動画表示
-cap = cv2.VideoCapture('/Users/nishimurakai/lab/Presentation_timer/data/ex.mov')
 
-model = load_model("MNIST.h5") # 学習済みモデルをロード
+engines = pyocr.get_available_tools()
+engine = engines[0]
+
+cap = cv2.VideoCapture('data/ex1.mp4')
+
+# model = load_model("MNIST.h5") # 学習済みモデルをロード
 
 # 無限ループ
 while(True):
 
-    # 判定用データの初期化
-    Xt = []
-    Yt = []
+    ret, frame = cap.read()    
 
-    ret, frame = cap.read()
-
-    if k == ord("q") or ret: # 終了処理
+    if not ret: # 終了処理
         break
 
     # 画像のサイズを取得,表示。グレースケールの場合,shape[:2]
     h, w, _ = frame.shape[:3]
 
-    # 画像の中心点を計算
-    w_center = w//2
-    h_center = h//2
+    # ページ番号をフォーカス
+    w_center = (w//100)*93
+    h_center = (h//1000)*999
 
-    # 画像の真ん中に142×142サイズの四角を描く
-    cv2.rectangle(frame, (w_center-71, h_center-71), (w_center+71, h_center+71),(255, 0, 0))
-
-    # カメラ画像の整形
-    im = frame[h_center-70:h_center+70, w_center-70:w_center+70] # トリミング
-    im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY) # グレースケールに変換
-    _, th = cv2.threshold(im, 0, 255, cv2.THRESH_OTSU) # 2値化
-    th = cv2.bitwise_not(th) # 白黒反転
-    th = cv2.GaussianBlur(th,(9,9), 0) # ガウスブラーをかけて補間
-    th = cv2.resize(th,(28, 28), cv2.INTER_CUBIC) # 訓練データと同じサイズに整形
-
-    Xt.append(th)
-    Xt = np.array(Xt)/255
-
-    result = model.predict(Xt, batch_size=1) # 判定,ソート
-    for i in range(10):
-        r = round(result[0,i], 2)
-        Yt.append([i, r])
-        Yt = sorted(Yt, key=lambda x:(x[1]))
+    # # カメラ画像の整形
+    im = frame[h_center:h_center+60, w_center-30:w_center+30] # トリミング
 
     # 判定結果を上位3番目まで表示させる 
-    print(Yt[9])
 
-    k =  cv2.waitKey(1) & 0xFF # キーが押下されるのを待つ。1秒置き。64ビットマシンの場合,& 0xFFが必要
+    cv2.imwrite('checkpt.png', im)
+    
+    num = engine.image_to_string(Image.open('checkpt.png'), lang='eng', builder=pyocr.builders.DigitBuilder(tesseract_layout=6))
+    
+    print(num) # 「Test Message」が出力される
+
+    if num.isdecimal():
+        p_num = int(num)
+    else:
+        p_num = -1
+
+    if p_num == -1:
+        continue
+    elif p_num in sec:
+        sec[p_num] += tick
+    else:
+        sec[p_num] = tick
+
+sec = sorted(sec.items())
+sec = dict((x, y) for x, y in sec)
+
+print(sec)
+
+sum = 0.0
+for i in sec.values():
+    sum += i
+
+print(sum)
 
 cap.release() # カメラを解放
 cv2.destroyAllWindows() # ウィンドウを消す
